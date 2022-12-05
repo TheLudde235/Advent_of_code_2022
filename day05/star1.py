39d77d08f4106a4e7c5b8c90eb48ae5289e09190
Ludde235 <ludde.lagerstrom@outlook.com>
2022-12-05 16:24:31 +0100

    completed day 5

--- day05/star1.py
+++ day05/star1.py
@@ -1,24 +1,15 @@
-def move(amount: int, f: int, t: int):
-    for x in range(amount):
+def move(a: int, f: int, t: int):
+    for x in range(a):
         removed_item = stacks_list[f - 1].pop(-1)
         stacks_list[t - 1].append(removed_item)
 
-
-inp = '''    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2'''.split('\n')
 
 inp = open('input.txt').read().split('\n')
 row_list: list = list()
 move_list: list = list()
 amount_of_stacks: int = 0
 moves_part = False
+
 for line in inp:
     if moves_part:
         if line:
@@ -52,4 +43,5 @@ for mo in move_list:
 out = ''
 for stack in stacks_list:
     out += stack[-1]
+    
 print(out)
--- day05/star2.py
+++ day05/star2.py
@@ -1,24 +1,23 @@
-def move(amount: int, f: int, t: int):
-    for x in range(amount):
-        removed_item = stacks_list[f - 1].pop(-1)
-        stacks_list[t - 1].append(removed_item)
+def pop_im(l: list, a: int) -> [list, list]:
+    o = []
+    for _ in range(a):
+        o.insert(0, l.pop())
+    return o, l
 
 
-inp = '''    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
+def move(a: int, f: int, t: int):
+    removed_items, non_removed_items = pop_im(stacks_list[f-1], a)
+    print(removed_items, non_removed_items)
+    stacks_list[f - 1] = non_removed_items
+    stacks_list[t - 1].extend(removed_items)
 
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2'''.split('\n')
 
-# inp = open('input.txt').read().split('\n')
+inp = open('input.txt').read().split('\n')
 row_list: list = list()
 move_list: list = list()
 amount_of_stacks: int = 0
 moves_part = False
+
 for line in inp:
     if moves_part:
         if line:
@@ -29,6 +28,7 @@ for line in inp:
             moves_part = True
         else:
             row_list.append(line)
+
 row_list.reverse()
 
 stacks_list: list[list[str]] = [[] for x in range(amount_of_stacks)]
@@ -40,7 +40,6 @@ for layer in row_list:
             stacks_list[i].append(layer[1 + i * 4])
 
 print(stacks_list)
-print(move_list)
 
 for mo in move_list:
     m = mo.split()
@@ -48,8 +47,11 @@ for mo in move_list:
     fr = int(m[3])
     to = int(m[5])
     move(amount, fr, to)
+    print(mo)
+    print(stacks_list)
 
 out = ''
 for stack in stacks_list:
     out += stack[-1]
+
 print(out)
