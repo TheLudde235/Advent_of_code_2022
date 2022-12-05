2364cf7ac790b5e7b42aff0027db38150a8138e3
Ludde235 <ludde.lagerstrom@outlook.com>
2022-12-05 07:47:24 +0100

    commiting before leaving

--- day05/star1.py
+++ day05/star1.py
@@ -7,7 +7,7 @@ move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2'''.split('\n')
-
+inp = open('input.txt').read().split('\n')
 row_list: list = list()
 amount_of_stacks: int = 0
 for line in inp:
@@ -22,8 +22,16 @@ current_stack = 0
 
 for layer in row_list:
     current_stack = 0
-    for stack in layer.split():
-        print(stack, current_stack)
-        stacks_list[current_stack].append(stack[1])
-        current_stack += 1
+    # print(layer)
+    for ch in layer:
+        print(ch)
+        if ch == ' ':
+            current_stack += 1
+        elif ch == '[' or ch == ']':
+            continue
+        else:
+            if current_stack > amount_of_stacks:
+                current_stack /= 4
+            print(current_stack)
+            stacks_list[int(current_stack)] += ch
 print(stacks_list)
