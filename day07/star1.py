9215d462d095bd5bfa02fb9c08fe845b96c62028
Ludvig <91793530+TheLudde235@users.noreply.github.com>
2022-12-07 20:45:59 +0100

    removed test input

--- day07/star1.py
+++ day07/star1.py
@@ -1,29 +1,3 @@
-# directories are dicts and files ints
-
-inp = '''$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k'''.split('\n')
-
 inp = open('input.txt').read().split('\n')
 class File:
     name: str
